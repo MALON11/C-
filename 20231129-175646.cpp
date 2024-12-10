28e81a9993e760bd716ff608d5a3465f230a2548
MALON11 <malonsumbizi@gmail.com>
2024-12-10 18:20:09 +0300

    Add files via upload

---
+++ 20231129-175646.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main( )
+
+
+{
+int age;
+float gpa;
+char gender;
+
+age=19;
+gender='M';
+gpa=4.7;
+
+printf("My name is malon\n Iam %dyear Itake 
+        \"computer scienc\",\n 
+         have %f my gender is \"%c\"")
+        ,age,gpa,gender;
+        
+        
+ }
+        
+        
+        
+        
+        
---
+++ jau sana.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+
+{
+float base;
+float height;
+float area;
+
+printf(*enter the base*);
+scanf("base");
+
+printf(*enter the height*);
+scanf("height");
+
+area=1/2*base*height;
+
+printf("the area is%f",area);
+
+return 0;
+
+}
